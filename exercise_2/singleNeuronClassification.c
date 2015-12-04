0dea72b93ab72602b348b13cbea3843de45bb228
MichaelGrupp <mg91@arcor.de>
2015-12-04 14:57:09 +0100

    bullshit

--- exercise_2/singleNeuronClassification.c
+++ exercise_2/singleNeuronClassification.c
@@ -1,7 +1,10 @@
-/* Your algorithm reads a number of 3 - valued training examples : each such example consists of two inputs("x" and "y" value) and a desired output value of + 1 or -1. The exact number of training examples is unknown, but you can safely assume you will read <= 1000.
-At some point your program will find a training example '0,0,0\n', (note the desired output of zero, which is invalid!).This indicates that the training data is completely read, and your program should start training the neuron.
+/* Your algorithm reads a number of 3 - valued training examples : each such example consists of two inputs("x" and "y" value)
+and a desired output value of + 1 or -1. The exact number of training examples is unknown, but you can safely assume you will read <= 1000.
+At some point your program will find a training example '0,0,0\n', (note the desired output of zero, which is invalid!).
+This indicates that the training data is completely read, and your program should start training the neuron.
 
-After training, your program continues to read 2 - valued evaluation data : for each such example your program should report the corresponding class (+1 or -1) followed by return ('\n') as output.
+After training, your program continues to read 2 - valued evaluation data : for each such example your program should report 
+the corresponding class (+1 or -1) followed by return ('\n') as output.
 */
 
 #include<stdio.h>
@@ -11,15 +14,16 @@ After training, your program continues to read 2 - valued evaluation data : for 
 
 #define DIM  3	//dimension of data
 #define N DIM-1 //max array index
+#define NUM_NEURONS 1 //number of neurons
 
-float randomFloat() { 
-	float r = (float)((rand()-rand()) % 5);
+double randomFloat() { 
+	double r = (double)(0.1+(rand()-rand()) % 1);
 	return r;
 }
 
 typedef struct Neuron {
-	float w[DIM]; //weight vector {w0 ... wDIM}
-	float eta; //learning rate
+	double w[DIM]; //weight vector {w0 ... wDIM}
+	double eta; //learning rate
 } Neuron;
 void initWeights(Neuron* neuron) {
 	for (int i = 0; i <= N; i++)
@@ -32,56 +36,90 @@ void printNeuron(Neuron* neuron) {
 	printf("\nlearning rate: %f \n", neuron->eta);
 }
 
-float scalarProd(const float* vec1, const float* vec2, int n) {
-	float result = 0.0;
+double scalarProd(const double* vec1, const double* vec2, int n) {
+	double result = 0.0;
 	for (int i = 0; i <= n; i++)
 		result += vec1[i] * vec2[i];
 	return result;
 }
-float* sumVec(const float* vec1, const float* vec2, int n) {
-	float* result = (float*)malloc(n*sizeof(float));
+double* vectorProd(const double* vec1, const double* vec2, int n) {
+	double* result = (double*)malloc(n*sizeof(double));
+	for (int i = 0; i <= n; i++)
+		result[i] = vec1[i] * vec2[i];
+	return result;
+}
+double* sumVec(const double* vec1, const double* vec2, int n) {
+	double* result = (double*)malloc(n*sizeof(double));
 	for (int i = 0; i <= n; i++)
 		result[i] = vec1[i] + vec2[i];
 	return result;
 }
-float* multiplyVec(const float* vec, float scalar, int n) {
-	float* result = (float*)malloc(n*sizeof(float));
+double* multiplyVec(const double* vec, double scalar, int n) {
+	double* result = (double*)malloc(n*sizeof(double));
 	for (int i = 0; i <= n; i++)
 		result[i] = vec[i] * scalar;
 	return result;
+}
+double* tanhVec(const double* vec, int n) {
+	double* result = (double*)malloc(n*sizeof(double));
+	for (int i = 0; i <= n; i++)
+		result[i] = tanh(vec[i]);
+	return result;
+}
+
+//activation function
+double f(Neuron* neuron, double* in) {
+	return tanh(scalarProd(neuron->w, in, N));
+}
+//derivative of activation function 1/(cosh(x)^2)
+double fdx(Neuron* neuron, double* in) {
+	return 1 / pow(cosh(scalarProd(neuron->w, in, N)), 2);
+}
+//error
+double error(double out, double t) {
+	return t - out;
 }
 
 //training step
 //the raw data has the form {x1, x2, t}
 //convert to form x={x0, x1, x2} and t=t
-void updateWeights(Neuron* neuron, const float* rawData) {
+double updateWeights(Neuron* neuron, const double* rawData) {
 	int t = rawData[N]; //desired class (-1, +1)
-	float x[DIM]; //actual input vector
+	double x[DIM]; //actual input vector
 	x[0] = 1; //dummy input for w0
 	for (int i = 1; i <= N; i++) {
 		x[i] = rawData[i - 1]; //copy feature vector from from rawData
 	}
-	//update step: w(k+1) = w(k) + eta*x*t
-	memcpy(neuron->w, sumVec(neuron->w, multiplyVec(multiplyVec(x, t, N), neuron->eta, N), N), DIM*sizeof(float));
+	//perceptron update step: w(k+1) = w(k) + eta*x*t
+	//memcpy(neuron->w, sumVec(neuron->w, multiplyVec(multiplyVec(x, t), neuron->eta)), DIM*sizeof(double));
+	//neuron update step: delta rule (w(k+1) = w(k) + (error*w(k) * (fdx(w(k)*in) * in)
+	double out = f(neuron, x);
+	double e = error(out, t);
+	double f_dx = fdx(neuron, &x);
+	double* a = vectorProd(multiplyVec(neuron->w, e, N), multiplyVec(neuron->w, f_dx, N), N);
+	memcpy(neuron->w, sumVec(neuron->w, (a), N) , DIM*sizeof(double));
+	return e;
 }
 
-//activation function for classification
-int activate(Neuron* neuron, const float* rawData) {
-	float x[DIM]; //actual input vector
+//activate the neuron
+double activate(Neuron* neuron, const double* rawData) {
+	double x[DIM]; //actual input vector
 	x[0] = 1; //dummy input for w0
 	for (int i = 1; i <= N; i++) {
 		x[i] = rawData[i-1]; //copy feature vector from from rawData
 	}
-	//check on which side of the decision boundary
-	float a = scalarProd(neuron->w, x, N);
+	//perceptron: check on which side of the decision boundary
+	/* double a = scalarProd(neuron->w, x);
 	if (a >= 0)
 		return 1;
 	else
-		return -1;
+		return -1; */
+	//neuron: out = f(net) = f(w*input) = tanh(w*input)
+	return f(neuron, x);
 }
 
-float* parseTrainingInputLine(const char* line) {
-	float result[DIM];
+double* parseTrainingInputLine(const char* line) {
+	double result[DIM];
 	int k = 0;
 	for (int i = 0; i <= N; i++) {
 		int j = 0;
@@ -99,8 +137,8 @@ float* parseTrainingInputLine(const char* line) {
 	return result;
 }
 
-float* parseTestInputLine(const char* line) {
-	float result[DIM - 1];
+double* parseTestInputLine(const char* line) {
+	double result[DIM - 1];
 	int k = 0;
 	for (int i = 0; i <= N-1; i++) {
 		int j = 0;
@@ -122,7 +160,7 @@ int main() {
 	srand(time(NULL));
 	Neuron neuron;
 	initWeights(&neuron);
-	neuron.eta = 0.3;
+	neuron.eta = 0.5;
 
 	FILE* file;
 	file = fopen("C:\\Users\\Michael\\Source\\Repos\\Computational_Intelligence\\exercise_2\\testInput10A.txt", "r");
@@ -131,8 +169,8 @@ int main() {
 
 	//read training data ("x1,x2,t")
 	char line[60];
-	float trainingData[1000][DIM];
-	float testData[1000][N];
+	double trainingData[1000][DIM];
+	double testData[1000][N];
 	int i = 0; int trainingDataSize = 0; int testDataSize = 0;
 	int trainingParsed = 0;
 	while (1) {
@@ -142,34 +180,41 @@ int main() {
 				i = 0;
 			}
 			else {
-				memcpy(trainingData[i], parseTrainingInputLine(&line), DIM*sizeof(float));
+				memcpy(trainingData[i], parseTrainingInputLine(&line), DIM*sizeof(double));
 				trainingDataSize++; i++;
 			}
 		}
 		else if (fgets(line, 60, file) && trainingParsed == 1) {
-			memcpy(testData[i], parseTestInputLine(&line), (DIM-1)*sizeof(float));
+			memcpy(testData[i], parseTestInputLine(&line), (DIM-1)*sizeof(double));
 			testDataSize++; i++;
 		}
 		else
 			break;
 	}
 
-	int trainingCycles = 100;
-	for (int i = 0; i <= trainingCycles; i++) {
+	int trainingCycles = 0;
+	double MSE = 1;
+	double eSum = 0;
+	while (MSE > 0.0001) {
 		for (int j = 0; j < trainingDataSize; j++) {
-			updateWeights(&neuron, trainingData[j]);
+			eSum += pow(updateWeights(&neuron, trainingData[j]), 2); //squared error
 		}
-		printNeuron(&neuron);
+
+		MSE = eSum * (0.5 / (NUM_NEURONS*trainingDataSize)); //mean square error (see BILHR doc by Peer)
+		printf("MSE: %f\n", MSE);
+		//printNeuron(&neuron);
+		eSum = 0;
+		trainingCycles++;
 	}
 	
 	//read test data ("x1,x2")
 	//neuron.w[0] = 0.2;
 	for (int i = 0; i <= testDataSize; i++) {
-		int res = activate(&neuron, testData[i]);
+		double res = activate(&neuron, testData[i]);
 		if(res == 1)
-			printf("+%d\n", res);
+			printf("+%f\n", res);
 		else
-			printf("%d\n", res);
+			printf("%f\n", res);
 	}
 
 
